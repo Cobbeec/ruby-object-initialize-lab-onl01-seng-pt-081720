5696f5d3cbe854cf18891ac98fb12c58b88668f5
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-06 18:01:52 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Dog
+  def initialize(name, dogs_breed = "Mutt")
+    @name = name
+    @breed = dogs_breed
+  end
+ def name 
+   @name 
+ end 
+  def breed
+    @breed
+  end
+end
+
+class Person 
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def name 
+    @name 
+  end 
+end 
